3fe482133fc7f9ea9de67bba29d8a37caa6d2a6c
kwnsyn <75299356+kwnsyn@users.noreply.github.com>
2023-04-23 13:58:30 +0900

    setting commit

---
+++ src/main/java/com/plani/back/PlaniBeApplication.java
@@ -0,0 +1,13 @@
+package com.plani.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaniBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaniBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plani/back/PlaniBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plani.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaniBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
