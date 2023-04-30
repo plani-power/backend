95daa54c4492e336ce2173c01d1c3c56a41e0d5a
KimJungEun0429 <ioneco1788@kakao.com>
2023-04-30 19:55:33 +0900

    Feat : "스케줄 목록 조회 기능 구현"
    스케줄 목록 조회 API 구현

--- src/main/java/com/plani/back/controller/ScheduleController.java
+++ src/main/java/com/plani/back/controller/ScheduleController.java
@@ -2,6 +2,7 @@ package com.plani.back.controller;
 
 import com.plani.back.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,13 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@Controller
 public class ScheduleController {
 
     @Autowired
     ScheduleService scheduleService;
 
-    @GetMapping(value = "/schedules")
+    @GetMapping(value = "schedules")
     public Map<String, Object> schedules(HttpServletRequest request, HttpServletResponse response)
     {
         Map<String, Object> result = new HashMap<>();
@@ -31,6 +32,6 @@ public class ScheduleController {
             result.put("message", "촤하하하하 실패!");
         }
 
-        return result;
+        return  result;
     }
 }
